8b9b16a9be35cb91bf8d2094962a976294c28550
omarabady <omarabady44@gmail.com>
2025-11-16 11:28:05 +0200

    Initialize desktop layout

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_admin_dashboard/widgets/adaptive_layout.dart';
+import 'package:responsive_admin_dashboard/widgets/desktop_layout.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => SizedBox(),
+        desktopLayout: (context) => DesktopLayout(),
+      ),
+    );
   }
 }
---
+++ lib/widgets/adaptive_layout.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout({
+    super.key,
+    required this.mobileLayout,
+    required this.tabletLayout,
+    required this.desktopLayout,
+  });
+  final WidgetBuilder mobileLayout, tabletLayout, desktopLayout;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            return mobileLayout(context);
+          } else if (constraints.maxWidth < 900) {
+            return tabletLayout(context);
+          } else {
+            return desktopLayout(context);
+          }
+        },
+      ),
+    );
+  }
+}
