7fc8a8ffcec327c4ce28102fe90cae0557cafcb7
omarabady <omarabady44@gmail.com>
2025-11-16 11:53:57 +0200

    Add app styles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,73 @@
+import 'dart:ui';
+
+abstract class AppStyle {
+  static final TextStyle styleRegular16 = TextStyle(
+    color: const Color(0xFF064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle styleRegular12 = TextStyle(
+    color: const Color(0xFFaaaaaa),
+    fontSize: 12,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle styleRegular14 = TextStyle(
+    color: const Color(0xFFaaaaaa),
+    fontSize: 14,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle styleMedium16 = TextStyle(
+    color: const Color(0xFF064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle styleMedium20 = TextStyle(
+    color: const Color(0xFFFFFFFF),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle styleSemiBold16 = TextStyle(
+    color: const Color(0xFF064060),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle styleSemiBold18 = TextStyle(
+    color: const Color(0xFF4EB7F2),
+    fontSize: 19,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle styleSemiBold20 = TextStyle(
+    color: const Color(0xFF064060),
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle styleSemiBold24 = TextStyle(
+    color: const Color(0xFF4EB7F2),
+    fontSize: 24,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle styleBold16 = TextStyle(
+    color: const Color(0xFF4EB7F2),
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+  );
+}
