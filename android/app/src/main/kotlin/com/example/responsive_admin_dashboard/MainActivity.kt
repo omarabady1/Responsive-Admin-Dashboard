2d35e64a27e28a23a233a0201b2c1acdd00dce9b
omarabady <omarabady44@gmail.com>
2025-11-16 10:24:45 +0200

    First commit

---
+++ android/app/src/main/kotlin/com/example/responsive_admin_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.responsive_admin_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_admin_dashboard/home_view.dart';
+
+void main() {
+  runApp(const AdminDashboard());
+}
+
+class AdminDashboard extends StatelessWidget {
+  const AdminDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeView(),
+    );
+  }
+}
+
