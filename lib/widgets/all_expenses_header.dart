4bdce020ba4472e6646cb09013f357754609a158
omarabady <omarabady44@gmail.com>
2025-11-22 16:50:42 +0200

    Add the drop down menu to the AllExpenses section

--- lib/widgets/all_expenses_header.dart
+++ lib/widgets/all_expenses_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../utils/app_styles.dart';
+import 'custom_drop_down_menu.dart';
 
 class AllExpensesHeader extends StatelessWidget {
   const AllExpensesHeader({
@@ -13,29 +14,7 @@ class AllExpensesHeader extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text('All Expenses', style: AppStyle.styleSemiBold20),
-        Container(
-          padding: const EdgeInsets.all(12),
-          decoration: ShapeDecoration(
-            color: Colors.white,
-            shape: RoundedRectangleBorder(
-              side: BorderSide(width: 1, color: const Color(0xFFF1F1F1)),
-              borderRadius: BorderRadius.circular(12),
-            ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Monthly', style: AppStyle.styleRegular16),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.keyboard_arrow_down,
-                  color: Color(0xFF064061),
-                ),
-              ),
-            ],
-          ),
-        ),
+        CustomDropDownMenu()
       ],
     );
   }
