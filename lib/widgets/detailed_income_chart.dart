0a54819c08694f04ac70b46044c4c8d43bf28f27
omarabady <omarabady44@gmail.com>
2025-11-24 12:19:40 +0200

    Create detailed income chart

---
+++ lib/widgets/detailed_income_chart.dart
@@ -0,0 +1,79 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_admin_dashboard/utils/app_styles.dart';
+
+class DetailedIncomeChart extends StatefulWidget {
+  const DetailedIncomeChart({super.key});
+
+  @override
+  State<DetailedIncomeChart> createState() => _DetailedIncomeChartState();
+}
+
+class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
+  int activeIndex = -1;
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(aspectRatio: 1, child: PieChart(getChartData()));
+  }
+
+  PieChartData getChartData() {
+    return PieChartData(
+      pieTouchData: PieTouchData(
+        enabled: true,
+        touchCallback: (p0, pieTouchResponse) {
+          setState(() {
+            activeIndex =
+                pieTouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+          });
+        },
+      ),
+      sectionsSpace: 0,
+      sections: [
+        PieChartSectionData(
+          title: activeIndex == 0 ? 'Design Service' : '40%',
+          titleStyle: activeIndex == 0
+              ? AppStyle.styleSemiBold16
+              : AppStyle.styleSemiBold16.copyWith(color: Colors.white),
+          titlePositionPercentageOffset: activeIndex == 0 ? 1.4 : null,
+          radius: activeIndex == 0 ? 50 : 45,
+          value: 40,
+          color: const Color(0xFF208BC7),
+          showTitle: true,
+        ),
+        PieChartSectionData(
+          titleStyle: activeIndex == 1
+              ? AppStyle.styleSemiBold16
+              : AppStyle.styleSemiBold16.copyWith(color: Colors.white),
+          title: activeIndex == 1 ? 'Design Production' : '25%',
+          titlePositionPercentageOffset: activeIndex == 1 ? -1.3 : null,
+          radius: activeIndex == 1 ? 50 : 45,
+          value: 25,
+          color: const Color(0xFF4DB7F2),
+          showTitle: true,
+        ),
+        PieChartSectionData(
+          titleStyle: activeIndex == 2
+              ? AppStyle.styleSemiBold16
+              : AppStyle.styleSemiBold16.copyWith(color: Colors.white),
+          title: activeIndex == 2 ? 'Product Royalty' : '20%',
+          titlePositionPercentageOffset: activeIndex == 2 ? 1.3 : null,
+          radius: activeIndex == 2 ? 50 : 45,
+          value: 20,
+          color: const Color(0xFF064060),
+          showTitle: true,
+        ),
+        PieChartSectionData(
+          titleStyle: activeIndex == 3
+              ? AppStyle.styleSemiBold16
+              : AppStyle.styleSemiBold16.copyWith(color:  const Color(0xFF064060)),
+          title: activeIndex == 3 ? 'Other' : '15%',
+          titlePositionPercentageOffset: activeIndex == 3 ? 1.5 : null,
+          radius: activeIndex == 3 ? 50 : 45,
+          value: 15,
+          color: const Color(0xFFE2DECD),
+          showTitle: true,
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_admin_dashboard/utils/app_styles.dart';
+import 'package:responsive_admin_dashboard/utils/size_config.dart';
+import 'package:responsive_admin_dashboard/widgets/detailed_income_chart.dart';
 import 'package:responsive_admin_dashboard/widgets/income_categories.dart';
 import 'package:responsive_admin_dashboard/widgets/income_chart.dart';
-
 import 'custom_drop_down_menu.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -20,16 +21,31 @@ class IncomeSection extends StatelessWidget {
         children: [
           IncomeSectionHeader(),
           SizedBox(height: 16),
-          Row(
+          IncomeSectionBody(),
+        ],
+      ),
+    );
+  }
+}
+
+class IncomeSectionBody extends StatelessWidget {
+  const IncomeSectionBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.sizeOf(context).width;
+    return (width < 1620 && width > SizeConfig.desktop)
+        ? Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: DetailedIncomeChart(),
+        )
+        : Row(
             children: [
               Expanded(child: IncomeChart()),
               SizedBox(width: 20),
               Expanded(flex: 2, child: IncomeCategories()),
             ],
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
 
